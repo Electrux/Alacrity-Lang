ca4793da7db83e24b5f12984c099f151274852e1
Electrux <ElectruxRedsworth@gmail.com>
2019-01-27 19:27:28 +0530

    Removed empty src/Functions/* files

--- src/Functions/builds.cpp
+++
@@ -1,9 +0,0 @@
-/*
-	Copyright (c) 2019, Electrux
-	All rights reserved.
-	Using the BSD 3-Clause license for the project,
-	main LICENSE file resides in project's root directory.
-
-	Please read that file and understand the license terms
-	before using or altering the project.
-*/
