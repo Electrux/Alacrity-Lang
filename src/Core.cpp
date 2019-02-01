503aa113bf403a058b1165738e43fd87dbe2b4bc
Electrux <ElectruxRedsworth@gmail.com>
2019-02-01 22:02:48 +0530

    In Core::Init(), AL_LIB_PATHS now sets to full current directory of lib

--- src/Core.cpp
+++ src/Core.cpp
@@ -31,7 +31,7 @@ int Core::Init()
 {
 	int res = OK;
 
-	if( !Env::SetVar( ALLibPaths(), "lib" ) ) {
+	if( !Env::SetVar( ALLibPaths(), FS::GetCurrentDir() + "/lib" ) ) {
 		std::cout << "Core::Init() failed: Unable to set core AL_LIB_PATHS env var!\n";
 		return ENV_SETVAR_FAILED;
 	}
