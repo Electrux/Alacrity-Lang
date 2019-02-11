41ae3a804b5f72886542dd55a4662e5fdc1d7699
Electrux <ElectruxRedsworth@gmail.com>
2019-02-11 10:21:43 +0530

    bootstrap.sh will now stop if any compilation step fails

--- bootstrap.sh
+++ bootstrap.sh
@@ -26,12 +26,12 @@ mkdir -p "buildfiles/src/Parser"
 find src -name "*.cpp" | grep -v "Modules" | grep -v "main.cpp" | while read -r src_file; do
 	echo "Compiling: $src_file ..."
 	$compiler -O2 -fPIC -std=c++17 -c $src_file -o buildfiles/$src_file.o -I/usr/local/include
-	if ! [[ $? == 0 ]]; then
+	if [[ $? != 0 ]]; then
 		break
 	fi
 done
 
-if ! [[ $? == 0 ]]; then
+if [[ $? != 0 ]]; then
 	echo "Error in compiling sources, will not continue"
 	exit $?
 fi
@@ -48,38 +48,65 @@ fi
 
 echo "Building binary: al..."
 $compiler -O2 -fPIC -std=c++17 -g -o buildfiles/al src/main.cpp $buildfiles -I/usr/local/include -L/usr/local/lib -ldl -lpthread $fs
+if [[ $? != 0 ]]; then
+	exit $?
+fi
 
 # Library: core
 echo "Building libray: core..."
 $compiler -O2 -fPIC -std=c++17 -shared -o buildfiles/libcore.so src/Modules/core.cpp $buildfiles -I/usr/local/include -L/usr/local/lib -ldl -lpthread $fs
+if [[ $? != 0 ]]; then
+	exit $?
+fi
 
 # Library: os
 echo "Building libray: os..."
 $compiler -O2 -fPIC -std=c++17 -shared -o buildfiles/libos.so src/Modules/os.cpp $buildfiles -I/usr/local/include -L/usr/local/lib -ldl -lpthread $fs
+if [[ $? != 0 ]]; then
+	exit $?
+fi
 
 # Library: string
 echo "Building libray: string..."
 $compiler -O2 -fPIC -std=c++17 -shared -o buildfiles/libstring.so src/Modules/string.cpp $buildfiles -I/usr/local/include -L/usr/local/lib -ldl -lpthread $fs
+if [[ $? != 0 ]]; then
+	exit $?
+fi
 
 # Library: math
 echo "Building libray: math..."
 $compiler -O2 -fPIC -std=c++17 -shared -o buildfiles/libmath.so src/Modules/math.cpp $buildfiles -I/usr/local/include -L/usr/local/lib -ldl -lpthread $fs
+if [[ $? != 0 ]]; then
+	exit $?
+fi
 
 # Library: list
 echo "Building libray: list..."
 $compiler -O2 -fPIC -std=c++17 -shared -o buildfiles/liblist.so src/Modules/list.cpp $buildfiles -I/usr/local/include -L/usr/local/lib -ldl -lpthread $fs
+if [[ $? != 0 ]]; then
+	exit $?
+fi
 
 # Library: audio
 echo "Building libray: audio..."
 $compiler -O2 -fPIC -std=c++17 -shared -o buildfiles/libaudio.so src/Modules/audio.cpp $buildfiles -I/usr/local/include -L/usr/local/lib -ldl -lpthread -lsfml-audio $fs
+if [[ $? != 0 ]]; then
+	exit $?
+fi
 
 # Library: project
 echo "Building libray: project..."
 $compiler -O2 -fPIC -std=c++17 -shared -o buildfiles/libproject.so src/Modules/project.cpp $buildfiles -I/usr/local/include -L/usr/local/lib -ldl -lpthread $fs
+if [[ $? != 0 ]]; then
+	exit $?
+fi
 
 # Library: builds
 echo "Building libray: builds..."
 $compiler -O2 -fPIC -std=c++17 -shared -o buildfiles/libbuilds.so src/Modules/builds.cpp src/Modules/builds/cxx.cpp src/Modules/builds/c.cpp $buildfiles -I/usr/local/include -L/usr/local/lib -ldl -lpthread $fs
+if [[ $? != 0 ]]; then
+	exit $?
+fi
 
 # Install this
 
